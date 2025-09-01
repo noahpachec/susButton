65e73ac3d0077081941e62d9f1dbf4768ade14ef
noahpachec <noahpachec@gmail.com>
2025-09-01 11:16:05 +1000

    added clickability and basic sound effect

--- scripts.js
+++ scripts.js
@@ -1,3 +1,21 @@
-function click() {
-    
+function buttonClick() {
+    changeBackground();
+    playSound();
+}
+
+
+function changeBackground() {
+    const body = document.getElementById("body");
+    randomNum = Math.random();
+    randomHexNum = "#" + (Math.floor(randomNum*16777215)).toString(16);
+    console.log(randomHexNum);
+    body.style.backgroundColor = randomHexNum;
+}
+
+
+function playSound() {
+    const soundFile = new Audio("./sounds/fart.mp3");
+
+    soundFile.currentTime = 0;
+    soundFile.play();
 }
